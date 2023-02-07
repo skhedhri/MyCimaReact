c1b6fa7a61b25e6212754a34452608133b009f9c
Saif KHEDHRI <s.khedhri21256@pi.tn>
2023-02-07 09:03:31 +0100

    first initial

---
+++ src/components/Header.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <nav>
+        <ul>
+          <NavLink
+            to="/"
+            className={(nav) => (nav.isActive ? "nav-active" : "")}
+          >
+            <li>Accueil</li>
+          </NavLink>
+          <NavLink
+            to="/coup-de-coeur"
+            className={(nav) => (nav.isActive ? "nav-active" : "")}
+          >
+            <li>Coup de coeur</li>
+          </NavLink>
+          <NavLink
+            to="/"
+            className={("")}
+          >
+            <li>Login</li>
+          </NavLink>
+        </ul>
+      </nav>
+      <h1>My Cima</h1>
+    </div>
+  );
+};
+
+export default Header;
